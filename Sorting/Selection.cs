8c78ddd1c41c30883f7dfe1cf584c6ebd6519861
Trong Tam <trongtam068@gmail.com>
2023-08-27 11:16:01 +0700

    Selection

--- Sorting/Selection.cs
+++ Sorting/Selection.cs
@@ -11,7 +11,13 @@ namespace Sorting
 
         /* Sắp xếp lựa chọn là một thuật toán sắp xếp một mảng trong đó nó lặp từ đầu vòng lặp và kiểm tra các phần tử khác để tìm giá trị tối thiểu.
          * Sau khi kết thúc lần lặp đầu tiên, giá trị tối thiểu được hoán đổi với phần tử hiện tại. Việc lặp lại sau đó tiếp tục từ phần tử thứ 2, v.v.
-         * link: https://youtube.com/playlist?list=PLprfEn_dJT0_z-A6FlUYjYJVLh4RrmoiZ&si=hPg9ZLn-JZ_t1lXw
+         *Độ phức tạp của thuật toán sắp xếp chọn (selection sort) là O(n^2), 
+         *trong đó n là số lượng phần tử trong danh sách cần sắp xếp.Thuật toán sắp xếp chọn có hai vòng lặp lồng nhau. 
+         *Vòng lặp bên ngoài chạy từ 0 đến n-1, và vòng lặp bên trong chạy từ i+1 đến n. 
+         *Với mỗi vòng lặp bên ngoài, cần thực hiện một số lần so sánh và hoán đổi vị trí. 
+         *Tổng số lần so sánh và hoán đổi vị trí là (n-1) + (n-2) + ... + 1 = n(n-1)/2, tương đương với O(n^2).
+         *Do đó, độ phức tạp của thuật toán sắp xếp chọn là O(n^2), nghĩa là thời gian thực hiện thuật toán tăng theo bình phương số lượng phần tử trong danh sách.
+         *link: https://youtube.com/playlist?list=PLprfEn_dJT0_z-A6FlUYjYJVLh4RrmoiZ&si=hPg9ZLn-JZ_t1lXw
          */
         static void Main(string[] args)
         {
